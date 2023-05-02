edf13717e9d6b81bd4b255bea3c4efbe4ce7fea7
lpang2143 <78526013+lpang2143@users.noreply.github.com>
2023-05-01 22:49:19 -0400

    init

---
+++ dash_app.py
@@ -0,0 +1,61 @@
+import dash
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+from navbar import create_navbar
+
+TITLE = "Dash Visualization"
+NAV = create_navbar()
+# FA = "https://use.fontawesome.com/releases/v6.2.1/css/all.css"
+
+
+dash_app = dash.Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    external_stylesheets = [
+        dbc.themes.MINTY,
+        # FA,
+    ],
+    title=TITLE,
+    use_pages=True,
+)
+
+'''
+<!DOCTYPE html>
+<html>
+    <head>
+        {{%metas%}}
+        <title>{APP_TITLE}</title>
+        {{%favicon%}}
+        {{%css%}}
+    </head>
+    <body>
+        {{%app_entry%}}
+        <footer>
+            {{%config%}}
+            {{%scripts%}}
+            {{%renderer%}}
+        </footer>
+        
+    </body>
+</html>
+'''
+
+dash_app.layout = dcc.Loading(  # <- Wrap App with Loading Component
+    id='loading_page_content',
+    children=[
+        html.Div(
+            [
+                NAV,
+                dash.page_container
+            ]
+        )
+    ],
+    color='primary',
+    fullscreen=True
+)
+
+server = dash_app.server
+
+if __name__ == '__main__':
+    dash_app.run_server(debug=False)
